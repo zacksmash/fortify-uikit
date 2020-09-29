d064d667edf7e4ea52d74deb72ab370e74603d97
zacksmash <zachary.warren@me.com>
2020-09-28 23:08:30 -0600

    Update layout file

--- stubs/resources/views/layouts/app.blade.php
+++ stubs/resources/views/layouts/app.blade.php
@@ -1,72 +1,89 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/manifest.js') }}"></script>
-        <script src="{{ mix('js/vendor.js') }}"></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-    </head>
+    <!-- Scripts -->
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
 
-    <body>
-        <div id="app">
-            <nav class="uk-navbar-container uk-margin" uk-navbar>
-                <div class="uk-navbar-left">
-                    <a class="uk-navbar-item uk-logo" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+</head>
 
-                <div class="uk-navbar-right">
-                    <ul class="uk-navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li>
-                                <a href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+<body>
+    <div id="app">
+        <nav class="uk-navbar-container uk-margin" uk-navbar>
+            <div class="uk-navbar-left">
+                <a class="uk-navbar-item uk-logo" href="{{ route('home') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
 
-                            @if (Route::has('register'))
-                                <li>
-                                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li>
-                                <a href="#">
-                                    <span uk-icon="icon: user;" class="uk-margin-small-right"></span>
-                                    Hi, {{ Auth::user()->name }}
-                                </a>
-                            </li>
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav">
+                    <li>
+                        <a href="#"><span uk-icon="grid"></span></a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li>
+                                        <a href="{{ route('login') }}">
+                                            <span uk-icon="sign-in" class="uk-margin-small-right"></span>
+                                            {{ __('Login') }}
+                                        </a>
+                                    </li>
 
-                            <li uk-tooltip="Logout">
-                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <span uk-icon="icon: sign-out"></span>
-                                </a>
+                                    @if (Route::has('register'))
+                                        <li>
+                                            <a href="{{ route('register') }}">
+                                                <span uk-icon="file-edit" class="uk-margin-small-right"></span>
+                                                {{ __('Register') }}
+                                            </a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="uk-nav-header">
+                                        <span uk-icon="user" class="uk-margin-small-right"></span>
+                                        Hi, {{ Auth::user()->name }}
+                                    </li>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" uk-hidden>
-                                    @csrf
-                                </form>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </nav>
+                                    <li class="uk-nav-divider"></li>
 
-            <main>
-                @yield('content')
-            </main>
-        </div>
+                                    <li uk-tooltip="Logout">
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            <span uk-icon="sign-out" class="uk-margin-small-right"></span>
+                                            Logout
+                                        </a>
 
-        <!-- App -->
-        <script src="{{ mix('js/app.js') }}"></script>
-    </body>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" uk-hidden>
+                                            @csrf
+                                        </form>
+                                    </li>
+                                @endguest
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <main>
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- App -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
 </html>
